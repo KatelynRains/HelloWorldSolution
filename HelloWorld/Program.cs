36fc0cbccb70216f96c243143ce21deeb35af205
Katelyn Rains <krains@tql.com>
2021-05-18 16:04:04 -0400

    c# work 5.18

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HelloWorld;
 
 namespace HelloWorld
 {
@@ -6,7 +7,23 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World in C#!");
+            var sarah = new Student();
+            sarah.Firstname = "Sarah";
+            sarah.Lastname = "George";
+            sarah.SetHireDate(2017, 6, 26);
+
+            var Charlie = new Student();
+            Charlie.Firstname = "Charlie";
+            Charlie.Lastname = "Traylor";
+
+            var Mattea = new Student();
+            Mattea.Firstname = "Mattea";
+            Mattea.Lastname = "Swain";
+
+            sarah.Print();
+            Charlie.Print();
+            Mattea.Print();
+            
         }
     }
 }
---
+++ HelloWorld/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Student
+    {   //property
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public bool Active { get; set; }
+        private DateTime HireDate { get; set; } //private: no other classes can use - only the Student class
+
+        //Methods
+
+        public void SetHireDate(int year, int month, int day)
+        { 
+            if (year < 1950 || year > 2200)  // || is 'OR'
+            {
+                Console.WriteLine($"Year must be between 1950 and 2200"); //looking for error and will display this message
+                return; //means to get out of the method if true
+            }  
+            if (month < 1 || month > 12) 
+            { 
+                Console.WriteLine($"Do you know what a calendar is??? 1 <= x <= 12");
+                return;
+            }
+            if (day < 1 || day > 31) 
+            { 
+                Console.WriteLine($"Invalid day!");
+                return;            
+             }
+            HireDate = new DateTime(year, month, day);
+        }
+        public void Print() // return type - void doesnt retun anything; methods always have ()
+        {
+            Console.WriteLine($"{Firstname} {Lastname} Hired: {HireDate.ToString("MM/dd/yyyy")}");
+        }   
+
+    }
+}
